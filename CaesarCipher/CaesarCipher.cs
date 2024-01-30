b1f2b228cc7c290850ca5041a75fe3cf32446791
Refaat Elabd <RefaatElabd7@gmail.com>
2024-01-30 09:35:39 +0200

    Caesar Cipher

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace CaesarCipher
+{
+    public class CaesarCipher
+    {
+        public static Dictionary<char, char> GenerateKey(int shift)
+        {
+            var domain = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+            var key = new Dictionary<char, char>();
+            for (var i = 0; i < domain.Length; i++)
+            {
+                key[domain[i]] = domain[(i + shift) % domain.Length];
+            }
+            return key;
+        }
+
+        public static string Encrypt(Dictionary<char, char> key, string value)
+        {
+            StringBuilder encrypedString = new();
+            for (var i = 0; i < value.Length; i++)
+            {
+                encrypedString.Append(key[value[i]]);
+            }
+            return encrypedString.ToString();
+        }
+
+        public static string Decrypt(Dictionary<char, char> key, string value)
+        {
+            StringBuilder decryptedString = new();
+            for (var i = 0; i < value.Length; i++)
+            {
+                decryptedString.Append(key.Keys.Where(k => key[k] == value[i]).First());
+            }
+            return decryptedString.ToString();
+        }
+    }
+}
---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,14 @@
+﻿using static CaesarCipher.CaesarCipher;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var key = GenerateKey(3);
+        var text = "Starting Cryptography".ToUpper();
+        var encrypted = Encrypt(key, text);
+        Console.WriteLine($"encrypted text is : {encrypted}");
+        var decrypted = Decrypt(key, encrypted);
+        Console.WriteLine($"decrypted text is : {decrypted}");
+    }
+}
