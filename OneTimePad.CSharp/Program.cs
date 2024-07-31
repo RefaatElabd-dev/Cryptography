a01da00cb21f57b9e7204a74eadd26e011e2cee6
Refaat Elabd <RefaatElabd7@gmail.com>
2024-07-31 08:11:33 +0300

    true randomness

--- OneTimePad.CSharp/Program.cs
+++ OneTimePad.CSharp/Program.cs
@@ -1,6 +1,19 @@
 ﻿using OneTimePad.CSharp;
+using static OneTimePad.TrueRandomness.TrueRandomness;
 
-XOR.Xor(4, 8);
-XOR.Xor(4, 4);
-XOR.Xor(255, 1);
-XOR.Xor(255, 127);
+// Generate a random number
+int randomNumber = GetRandomInt();
+Console.WriteLine("Random Number: " + randomNumber);
+
+// Generate a random byte array
+byte[] randomBytes = GetRandomBytes(16); // 16 bytes
+Console.WriteLine("Random Bytes: " + BitConverter.ToString(randomBytes));
+
+
+
+
+
+//XOR.Xor(4, 8);
+//XOR.Xor(4, 4);
+//XOR.Xor(255, 1);
+//XOR.Xor(255, 127);
---
+++ OneTimePad.CSharp/TrueRandomness.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace OneTimePad.TrueRandomness
+{
+    public class TrueRandomness
+    {
+        public static int GetRandomIntFromRNG()
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomNumber = new byte[4]; // 4 bytes for an integer
+                rng.GetBytes(randomNumber);
+                return BitConverter.ToInt32(randomNumber, 0);
+            }
+        }
+
+        public static byte[] GetRandomBytesFromRNG(int length)
+        {
+            using(RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomBytes = new byte[length];
+                rng.GetBytes(randomBytes);
+                return randomBytes;
+            }
+        }
+
+        public static int GetRandomInt()
+        {
+            byte[] randomNumber = new byte[4]; // 4 bytes for an integer
+            RandomNumberGenerator.Fill(randomNumber);
+            return BitConverter.ToInt32(randomNumber, 0);
+        }
+
+        public static byte[] GetRandomBytes(int length)
+        {
+            byte[] randomBytes = new byte[length];
+            RandomNumberGenerator.Fill(randomBytes);
+            return randomBytes;
+        }
+    }
+}
